46fd23390ea5b7d3952bca068f7f0790ad8c9303
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-04-04 11:31:57 +0530

    Add item to array with template variable.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,38 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
-
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created!';
-  serverName = 'TestServer';
-  serverCreated = false;
-  newItemStatus = false;
-  newItemName = '';
-  txtFruits = '';
+  // allowNewServer = false;
+  // serverCreationStatus = 'No server was created!';
+  // serverName = 'TestServer';
+  // serverCreated = false;
+  // newItemStatus = false;
+  // newItemName = '';
+  // txtFruits = '';
   fruits: string[];
-  servers = ['Test Server', 'Test Server 2']
+  // servers = ['Test Server', 'Test Server 2']
 
   constructor() {
-    this.fruits = ['oranges','apple'];
-    setTimeout(()=> {
-      this.allowNewServer = true;
+    this.fruits = ['oranges', 'apple'];
+    // setTimeout(() => {
+    //   this.allowNewServer = true;
 
-    },2000);
+    // }, 2000);
   }
 
-  ngOnInit() {
-  }
-  onCreateServer() {
-    this.serverCreated = true;
-    this.servers.push(this.serverName);
-    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
-  }
-  getNewItem(event, txtFruits: HTMLInputElement){
-    this.newItemStatus = true;
-    this.newItemName = 'Inserted';
+  ngOnInit() {}
+
+  // onCreateServer() {
+  //   this.serverCreated = true;
+  //   this.servers.push(this.serverName);
+  //   this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  // }
+
+  getNewItem(txtFruits: HTMLInputElement) {
+    // this.newItemStatus = true;
+    // this.newItemName = 'Inserted';
     this.fruits.push(txtFruits.value);
-    console.log(event);
   }
-
-
 }
